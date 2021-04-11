c946c10951ea50be8c63179a277f9ef67f79e52d
o2sevruk <o2sevruk@gmail.com>
2021-04-11 16:43:27 +0300

    Template done

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    'no-use-before-define': 'off',
+    'no-shadow': 'off',
+  },
+};
